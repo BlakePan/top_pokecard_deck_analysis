2bb4e93951790e9e7b421a59388322fedeb68bdb
blakepanCin <blake.pan@cinnamon.is>
2022-11-23 18:13:40 +0800

    refactor

--- parse_deck.py
+++ parse_deck.py
@@ -15,9 +15,19 @@ def extract_card(cards):
         if '（' in c[0]:
             loc = c[0].find('（')
             c[0] = c[0][:loc]
-        _dict[c[0]] = c[1][:-1]
+        _dict[c[0]] = int(c[-1][:-1])
     
     return _dict
+
+
+def find_category(all_categories, pokemon_dict):
+    # identify this deck belongs to which category (TODO: need more rules)
+    category = "others"
+    for c in all_categories[:-1]:
+        if c in pokemon_dict.keys():
+            category = c
+
+    return category
 
 
 def parse_deck(deck_code: str = None, deck_link: str = None):
@@ -51,7 +61,7 @@ def parse_deck(deck_code: str = None, deck_link: str = None):
             """
             cards = e.text.split('\n')
             for i in range(1, len(cards), 4):
-                pokemon_dict[cards[i]] = cards[i+3][:-1]
+                pokemon_dict[cards[i]] = int(cards[i+3][:-1])
         else:
             """
             example:
@@ -75,22 +85,56 @@ def parse_deck(deck_code: str = None, deck_link: str = None):
     return pokemon_dict, tool_dict, supporter_dict, stage_dict, energy_dict
 
 
+def parse_event_to_deck(event_link: str, num_people: int, decks: dict, all_categories: list):
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10) # seconds
+    driver.get(event_link)
+    date_str = driver.find_element(By.CLASS_NAME, "date-day").text
+    deck_elems = driver.find_elements(By.CLASS_NAME, "c-rankTable-row")
+    for deck_elem in deck_elems:
+        deck_link = deck_elem.find_element(By.CLASS_NAME, "deck").find_element(By.TAG_NAME, "a").get_property("href")
+        deck_code = deck_link.split("/")[-1]
+        pokemon_dict, tool_dict, supporter_dict, stage_dict, energy_dict = parse_deck(deck_link = deck_link)
+        category = find_category(all_categories, pokemon_dict)
+        rank = int(deck_elem.find_element(By.TAG_NAME, "td").get_attribute("class").split("-")[-1])
+
+        if category not in decks:
+            decks[category] = []
+
+        decks[category].append(
+            {
+                "deck_link": deck_link,
+                "deck_code": deck_code,
+                "pokemons": pokemon_dict,
+                "tools": tool_dict,
+                "supporters": supporter_dict,
+                "stages": stage_dict,
+                "energies": energy_dict,
+                "rank": rank,
+                "num_people": num_people,
+                "date": date_str
+            }
+        )
+
+    driver.close()
+
+
 if __name__ == "__main__":
     pokemon_dict, tool_dict, supporter_dict, stage_dict, energy_dict = \
     parse_deck(deck_code = "gngLgL-7AWHa3-LNgNnn")
-    print("--- use deck code")
+    parse_deck(deck_link = "https://www.pokemon-card.com/deck/confirm.html/deckID/gngLgL-7AWHa3-LNgNnn")
+    print("parse_deck():")
     print("pokemon_dict"); print(pokemon_dict)
     print("tool_dict"); print(tool_dict)
     print("supporter_dict"); print(supporter_dict)
     print("stage_dict"); print(stage_dict)
     print("energy_dict"); print(energy_dict)
+    print("\n")
 
-#     pokemon_dict, tool_dict, supporter_dict, stage_dict, energy_dict = \
-#     parse_deck(deck_link = "https://www.pokemon-card.com/deck/confirm.html/deckID/KcDc84-6NuRAE-8x848x")
-# #     parse_deck(deck_link = "https://www.pokemon-card.com/deck/confirm.html/deckID/NnngQg-WZuTqi-giLn9L")
-#     print("--- use deck link")
-#     print("pokemon_dict"); print(pokemon_dict)
-#     print("tool_dict"); print(tool_dict)
-#     print("supporter_dict"); print(supporter_dict)
-#     print("stage_dict"); print(stage_dict)
-#     print("energy_dict"); print(energy_dict)
+    event_link = "https://players.pokemon-card.com/event/detail/31798/result"
+    num_people = 32
+    all_categories = ["ルギアVSTAR", "ミュウVMAX", "ジュラルドンVMAX", "others"]
+    decks = {}
+    parse_event_to_deck(event_link, num_people, decks, all_categories)
+    print("parse_event_to_deck()")
+    print(decks)
